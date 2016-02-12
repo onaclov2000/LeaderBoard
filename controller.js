3f04a7fca3bac5d70a6135e9677e2582643d3fe4
Tyson Bailey <onaclov2000+github@gmail.com>
2016-02-12 13:21:20 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -8,4 +8,58 @@ angular.module('myapp', ['firebase'])
             var sync = $firebase(ref);
             $scope.todos = sync.$asObject();
         }
-    ]);
+    ]).filter("textFilter", function() {
+    return function(input,text) {
+      var sorted = [];
+      
+      if (input) {
+//         console.log(input);
+        if (!input.$getIndex || typeof input.$getIndex != "function") {
+          // input is not an angularFire instance
+          if (angular.isArray(input)) {
+            console.log("Array");
+            // If input is an array, copy it
+            sorted = input.slice(0);
+          } else if (angular.isObject(input)) {
+            // If input is an object, map it to an array
+            angular.forEach(input, function(prop) {
+              console.log(prop);
+              console.log(text);
+              if( text && text.length > 0){
+                 if (prop['user'].indexOf(text) > -1){
+                    sorted.push(prop);
+               }
+              }
+              else{
+                 sorted.push(prop);
+              }
+            });
+          }
+        } else {
+          console.log("ELSE");
+          // input is an angularFire instance
+          var index = input.$getIndex();
+          if (index.length > 0) {
+            for (var i = 0; i < index.length; i++) {
+              var val = input[index[i]];
+              if (val) {
+                val.$id = index[i];
+                
+              if( text && text.length > 0 && val.body.indexOf(text) > -1){
+                 sorted.push(val);
+              }else{
+               
+                if(!text || text.length === 0 ){
+                   sorted.push(val);
+                 }
+              }
+                //
+              }
+            }
+          }
+        }
+      }
+      
+      return sorted;
+    };
+  });
